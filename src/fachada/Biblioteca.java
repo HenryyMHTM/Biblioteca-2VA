413a10558362b24a516c8e45a26d9ab552ff9d90
joyeuxpierrot <mario.ramon@ufape.edu.br>
2026-03-08 23:16:08 -0300

    Atualização das classes e interface de Biblioteca

--- src/fachada/Biblioteca.java
+++ src/fachada/Biblioteca.java
@@ -1,17 +1,41 @@
 package fachada;
-import dados.usuario.*;
-import negocio.entidades.Usuario;
 
-// fachada concentrando tudo pro main
+import dados.livro.RepositorioLivroCSV;
+import dados.usuario.RepositorioUsuariosCSV;
+import java.util.List;
+import negocio.*;
+import negocio.entidades.*;
+import negocio.excecao.*;
+
 public class Biblioteca implements IBiblioteca {
-    private IRepositorioUsuarios repUsuarios;
+    private final NegocioUsuario nUsuario;
+    private final NegocioLivro nLivro;
 
     public Biblioteca() {
-        // dps mudar pro CSV quando for salvar msm
-        this.repUsuarios = new RepositorioUsuariosArrayList();
+        this.nUsuario = new NegocioUsuario(new RepositorioUsuariosCSV());
+        this.nLivro = new NegocioLivro(new RepositorioLivroCSV());
     }
 
-    public void cadastrarUsuario(Usuario u) {
-        repUsuarios.cadastrarUsuario(u);
+    @Override
+    public void cadastrarUsuario(Usuario u) throws UsuarioJaExisteException {
+        nUsuario.cadastrar(u);
     }
+
+    @Override
+    public Usuario login(String cpf, String dataNasc) throws UsuarioNaoExisteException {
+        return nUsuario.efetuarLogin(cpf, dataNasc);
+    }
+
+    @Override
+    public List<Livro> listarLivros() {
+        return nLivro.listarDisponiveis();
+    }
+
+    @Override
+    public void emprestar(String isbn, Usuario u) throws Exception {
+        nUsuario.validarSituacao(u);
+        nLivro.emprestarLivro(isbn, u);
+    }
+
+    
 }
--- src/fachada/IBiblioteca.java
+++ src/fachada/IBiblioteca.java
@@ -1,5 +1,17 @@
 package fachada;
 
-public interface IBiblioteca {
+import java.util.List;
+import negocio.entidades.Livro;
+import negocio.entidades.Usuario;
+import negocio.excecao.*;
 
+public interface IBiblioteca {
+    // Cadastro e Autenticação
+    void cadastrarUsuario(Usuario u) throws UsuarioJaExisteException;
+    Usuario login(String cpf, String dataNasc) throws UsuarioNaoExisteException;
+
+    // Operações de Acervo
+    List<Livro> listarLivros(); //
+    void emprestar(String isbn, Usuario u) throws Exception;
+    
 }
